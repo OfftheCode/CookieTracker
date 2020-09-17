d4137f98b22fe00b4e7abad250882d3274841a53
OfftheCode <offthecode@gmail.com>
2020-09-17 19:47:30 +0200

    Initial Commit

---
+++ Cookie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cookie Tracker
+//
+//  Created by Piotr Szadkowski on 17/09/2020.
+//  Copyright © 2020 Piotr Szadkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
